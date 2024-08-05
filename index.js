4121a3a075d8d15b6c96e51d21ed2f7fdb6266a1
Subrata Sarkar <tccdcc8@gmail.com>
2024-08-06 01:54:58 +0600

    m

--- index.js
+++ index.js
@@ -2,18 +2,50 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 const app = express();
-var jwt = require("jsonwebtoken");
-const port = process.env.PORT || 8989;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve static files
 
 // Define a simple route
 app.get("/", (req, res) => {
   res.send("Hi Developer Server Is Running");
 });
+
+// Multer configuration for file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = path.join(__dirname, "uploads");
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage,fileFilter: (req, file, cb)=>{
+  const supportImages= /jpg | png/;
+  const extension= path.extname(file.originalname)
+  if(supportImages.test(extension)){
+    cb(null, true)
+  }else{
+    cb(new Error('please give Jpg / png file '))
+  }
+},
+limits:{
+  fileSize:5000000
+}
+ });
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dtcwl7u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -27,297 +59,173 @@ const client = new MongoClient(uri, {
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.send({ message: "You have no authHeader" });
+    return res.status(401).send({ message: "No authorization header" });
   }
   const token = authHeader.split(" ")[1];
-  jwt.verify(token, "secret", function (err, user) {
+  jwt.verify(token, "secret", (err, user) => {
     if (err) {
-      return res.send({ message: "Your token is not valid" });
-    } else {
-      req.user = user;
-      next();
+      return res.status(401).send({ message: "Invalid token" });
     }
+    req.user = user;
+    next();
   });
 };
 
 async function run() {
   try {
-    client.connect();
-    const digitalFurnitureDb = client.db("digitalFurnitureDb");
-    const productCollection =
-      digitalFurnitureDb.collection("productCollection");
-    const userCollection = digitalFurnitureDb.collection("userCollection");
-    const blogCollection = digitalFurnitureDb.collection("blogCollection");
-    const reviewCollection = digitalFurnitureDb.collection("reviewCollection");
+    await client.connect();
+    const bariVaraDB = client.db("bariVaraDB");
+    const propertyCollection = bariVaraDB.collection("propertyCollection");
+    const userCollection = bariVaraDB.collection("userCollection");
 
     app.post("/jwt", (req, res) => {
       const userInfo = req.body;
       const token = jwt.sign(userInfo, "secret", { expiresIn: "7d" });
       res.send({ token });
     });
+
     app.get("/states", verifyToken, async (req, res) => {
-      const products = await productCollection.estimatedDocumentCount();
-      const blogs = await blogCollection.estimatedDocumentCount();
-      const reviews = await reviewCollection.estimatedDocumentCount();
+      const properties = await propertyCollection.estimatedDocumentCount();
       const users = await userCollection.estimatedDocumentCount();
-      return res.send({ products, blogs, reviews, users });
+      res.send({ properties, users });
     });
 
-    // products curd
-
-    app.get("/products/email", async (req, res) => {
+    // Properties CRUD
+    app.get("/properties/email", verifyToken, async (req, res) => {
       const email = req.query.email;
       if (!email) {
-        return res.send({ message: "Please Login then Try" });
+        return res.status(400).send({ message: "Please login then try" });
       }
-      const query = { authorEmail: email };
-      const result = await productCollection.find(query).toArray();
-      return res.send(result);
+      const query = { owner: email };
+      const result = await propertyCollection.find(query).toArray();
+      res.send(result);
     });
 
-    app.get("/products/categories/:category", async (req, res) => {
-      const category = req.params.category;
-      if (!category) {
-        return res.send({ message: "category Notfound" });
-      }
-      const query = { category };
-      const result = await productCollection.find(query).toArray();
-      return res.send(result);
-    });
-    app.get("/products/:id", async (req, res) => {
+    app.get("/properties/:id", async (req, res) => {
       const id = req.params.id;
       if (!id) {
-        return res.send({ message: "Id invalid" });
+        return res.status(400).send({ message: "Invalid ID" });
       }
       const query = { _id: new ObjectId(id) };
-      const result = await productCollection.findOne(query);
-      return res.send(result);
+      const result = await propertyCollection.findOne(query);
+      res.send(result);
     });
-    app.patch("/products/:id", verifyToken, async (req, res) => {
+
+    app.patch("/properties/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       if (!id) {
-        return res.send({ message: "Id invalid" });
+        return res.status(400).send({ message: "Invalid ID" });
       }
       const filter = { _id: new ObjectId(id) };
       const product = req.body;
-      const result = await productCollection.updateOne(filter, {
-        $set: product,
-      });
-      return res.send(result);
-    });
-    app.delete("/products/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      if (!id) {
-        return res.send({ message: "Id invalid" });
-      }
-      const filter = { _id: new ObjectId(id) };
-      const result = await productCollection.deleteOne(filter);
-      return res.send(result);
-    });
-    app.get("/products", async (req, res) => {
-      const searchText = req.query.searchText;
-      let result = [];
-      if (searchText) {
-        result = await productCollection
-          .find({ title: { $regex: searchText, $options: "i" } })
-          .toArray();
-      } else {
-        result = await productCollection.find({}).toArray();
-      }
+      const result = await propertyCollection.updateOne(filter, { $set: product });
       res.send(result);
     });
 
-    app.post("/products", verifyToken, async (req, res) => {
-      const product = req.body;
-      if (!product) {
-        return res.send({ message: "No data for insert" });
-      }
-      const result = await productCollection.insertOne(product);
-      return res.send(result);
-    });
-
-    // Blogs curd
-    app.get("/blogs/:id", async (req, res) => {
+    app.delete("/properties/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       if (!id) {
-        return res.send({ message: "Id invalid" });
-      }
-      const query = { _id: new ObjectId(id) };
-      const result = await blogCollection.findOne(query);
-      return res.send(result);
-    });
-    app.get("/blogs/me/:email", async (req, res) => {
-      const email = req.params.email;
-      if (!email) {
-        return res.send({ message: "email not found" });
-      }
-      const query = { authorEmail: email };
-      const result = await blogCollection.find(query).toArray();
-      return res.send(result);
-    });
-    app.patch("/blogs/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      if (!id) {
-        return res.send({ message: "Id invalid" });
+        return res.status(400).send({ message: "Invalid ID" });
       }
       const filter = { _id: new ObjectId(id) };
-      const blog = req.body;
-      const result = await blogCollection.updateOne(filter, {
-        $set: blog,
-      });
-      return res.send(result);
-    });
-    app.delete("/blogs/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      if (!id) {
-        return res.send({ message: "Id invalid" });
-      }
-      const filter = { _id: new ObjectId(id) };
-      const result = await blogCollection.deleteOne(filter);
-      return res.send(result);
-    });
-    app.get("/blogs", async (req, res) => {
-      const searchText = req.query.searchText;
-      let result = [];
-      if (searchText) {
-        result = await blogCollection
-          .find({ title: { $regex: searchText, $options: "i" } })
-          .toArray();
-      } else {
-        result = await blogCollection.find({}).toArray();
-      }
+      const result = await propertyCollection.deleteOne(filter);
       res.send(result);
     });
-    app.post("/blogs", verifyToken, async (req, res) => {
-      const blog = req.body;
-      if (!blog) {
-        return res.send({ message: "No data for insert" });
+
+    app.get("/properties", async (req, res) => {
+      try {
+        const { division, district, upazila, postOffice, type } = req.query;
+        let query = {};
+        if (division) query.division = division;
+        if (district) query.district = district;
+        if (upazila) query.upazila = upazila;
+        if (postOffice) query.postOffice = postOffice;
+        if (type) query.type = type;
+        
+        const result = await propertyCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
       }
-      const result = await blogCollection.insertOne(blog);
-      return res.send(result);
     });
 
-    // reviews curd
-    app.get("/reviews/email", async (req, res) => {
-      const email = req.query.email;
-      if (!email) {
-        return res.send({ message: "Please login then try" });
+    app.post("/properties", verifyToken, upload.array("images", 6), async (req, res) => {
+      try {
+        const { amenities, ...other } = req.body;
+        const images = req.files.map((file) => file.path);
+        const propertyDoc = {
+          ...other,
+          images,
+          amenities: amenities.split(",").map((amenity) => amenity.trim()),
+        };
+
+        const result = await propertyCollection.insertOne(propertyDoc);
+        res.status(201).json(result);
+      } catch (error) {
+        res.status(400).json({ error: error.message });
       }
-      const query = { authorEmail: email };
-      const result = await reviewCollection.find(query).toArray();
-      return res.send(result);
-    });
-    app.get("/reviews/:id", async (req, res) => {
-      const id = req.params.id;
-      if (!id) {
-        return res.send({ message: "Id invalid" });
-      }
-      const query = { _id: new ObjectId(id) };
-      const result = await reviewCollection.findOne(query);
-      return res.send(result);
     });
 
-    app.patch("/reviews/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      if (!id) {
-        return res.send({ message: "Id invalid" });
-      }
-      const filter = { _id: new ObjectId(id) };
-      const review = req.body;
-      const result = await reviewCollection.updateOne(filter, {
-        $set: review,
-      });
-      return res.send(result);
-    });
-    app.delete("/reviews/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      if (!id) {
-        return res.send({ message: "Id invalid" });
-      }
-      const filter = { _id: new ObjectId(id) };
-      const result = await reviewCollection.deleteOne(filter);
-      return res.send(result);
-    });
-    app.get("/reviews", async (req, res) => {
-      const searchText = req.query.searchText;
-      let result = [];
-      if (searchText) {
-        result = await reviewCollection
-          .find({ description: { $regex: searchText, $options: "i" } })
-          .toArray();
-      } else {
-        result = await reviewCollection.find({}).toArray();
-      }
-      res.send(result);
-    });
-    app.post("/reviews", verifyToken, async (req, res) => {
-      const review = req.body;
-      if (!review) {
-        return res.send({ message: "No data for insert" });
-      }
-      const result = await reviewCollection.insertOne(review);
-      return res.send(result);
-    });
-
-    // users curd
+    // Users CRUD
     app.get("/users/get/:id", async (req, res) => {
       const id = req.params.id;
       if (!id) {
-        return res.send({ message: "Id invalid" });
+        return res.status(400).send({ message: "Invalid ID" });
       }
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.findOne(query);
-      return res.send(result);
+      res.send(result);
     });
+
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       if (!email) {
-        return res.send({ message: "Email not fond" });
+        return res.status(400).send({ message: "Email not found" });
       }
       const query = { email };
       const result = await userCollection.findOne(query);
-      return res.send(result);
+      res.send(result);
     });
+
     app.patch("/users/:id", async (req, res) => {
       const id = req.params.id;
       if (!id) {
-        return res.send({ message: "Id invalid" });
+        return res.status(400).send({ message: "Invalid ID" });
       }
       const filter = { _id: new ObjectId(id) };
       const newDoc = req.body;
-      const result = await userCollection.updateOne(filter, {
-        $set: newDoc,
-      });
-      return res.send(result);
+      const result = await userCollection.updateOne(filter, { $set: newDoc });
+      res.send(result);
     });
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       if (!id) {
-        return res.send({ message: "Id invalid" });
+        return res.status(400).send({ message: "Invalid ID" });
       }
       const filter = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(filter);
-      return res.send(result);
+      res.send(result);
     });
 
-    //verify admin
     app.get("/users", verifyToken, async (req, res) => {
       const query = {};
       const result = await userCollection.find(query).toArray();
       res.send(result);
     });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       if (!user) {
-        return res.send({ message: "Id invalid" });
+        return res.status(400).send({ message: "Invalid data" });
       }
       const query = { email: user.email };
       const exist = await userCollection.findOne(query);
       if (exist) {
-        return res.send({ message: "user already exist" });
+        return res.status(400).send({ message: "User already exists" });
       }
       const result = await userCollection.insertOne({ ...user, role: "user" });
-      return res.send(result);
+      res.send(result);
     });
   } finally {
     // Ensures that the client will close when you finish/error
@@ -327,4 +235,6 @@ async function run() {
 run().catch(console.dir);
 
 // Start the server
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
